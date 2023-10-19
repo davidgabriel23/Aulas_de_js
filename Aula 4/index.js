dba543816b26ef73ee5e37423dffa1fe723d3e82
davidgabriel23 <daviid.gabriel25@gmail.com>
2023-10-19 09:01:08 -0300

    Variáveis com let

---
+++ Aula 4/index.js
@@ -0,0 +1,30 @@
+//veriaveis/ Duas maneiras de declarar uma variavel usando let ou var so que o var é mais antigo o mais usado é let 
+
+let nome = "David"; // testo dentro de aspas é um string
+
+
+
+console.log(nome, 'nasceu em 1996'); // quando for declarar a variavel na frase sempre colocar virgula na frente 
+console.log('em 2000', nome, 'conheceu jessica');
+console.log(nome, 'casou-se com Jessica em 2024');
+console.log('Jessica teve 1 uma filha com', nome, 'em 2026');
+console.log('A filha de', nome, 'se chama Clara');
+
+
+// declarando uma variavel sem valor 
+
+let name; // declarando a variavel
+nome = 'qualquer valor '; // inicializando a variavel
+console.log(nome)
+nome = 'david'
+console.log(nome)
+
+
+// Não podemos criar Variáveis com palavras reservadas
+// Variáveis precisam ter nomes siginificativos
+// Não pode começar o nome de uma Variáveis com numero 
+// Na variavel não pode conter espaço  ou traços
+// Utilizamos camelCase - inicia com letra minuscula depois com a letra maiuscula
+// As Variáveis são Case-sensitive tem diferença entre letras minusculas e maiusculas
+// Não utiliza var sempre utiliza let 
+
