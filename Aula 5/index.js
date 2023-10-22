35141e9f22391b30e9b3872bfd208471458ce4c8
David Gabriel <daviid.gabriel25@gmail.com>
2023-10-22 11:11:49 -0300

    introdução ao const

---
+++ Aula 5/index.js
@@ -0,0 +1,22 @@
+// constantes
+
+
+
+// Não podemos criar constantes com palavras reservadas
+// constantes precisam ter nomes siginificativos
+// Não pode começar o nome de uma constantes com numero 
+// Na constantes não pode conter espaço  ou traços
+// Utilizamos camelCase - inicia com letra minuscula depois com a letra maiuscula
+// As constantes são Case-sensitive tem diferença entre letras minusculas e maiusculas
+// Não utiliza var sempre utiliza const
+
+
+const name1 = 'David';
+console.log(name1)
+
+// Oque nao pode fazer em uma constante é mudar o valor dela 
+
+const primeiroNumero = 5;
+const segundoNumero = 10; 
+const conta = primeiroNumero * segundoNumero
+console.log(conta)
